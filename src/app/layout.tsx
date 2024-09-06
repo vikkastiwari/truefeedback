a88a83e1a4f03957597dc72765d6cc991d46c406
Vikas Tiwari <vikas.tiwari@newfold.com>
2024-09-06 19:38:51 +0530

    provider updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import AuthProvider from "@/context/AuthProviders";
+import AuthProvider from "@/context/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
