fb4e6b3533da885a0924e5198fc3564dd5d799bc
Vikas Tiwari <vikas.tiwari@newfold.com>
2024-09-15 12:21:15 +0530

    multiple rerender issue fixed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,31 +1,30 @@
-import { NextRequest, NextResponse } from 'next/server'
-export { default } from "next-auth/middleware"
-import { getToken } from "next-auth/jwt"
+import { NextRequest, NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+export { default } from 'next-auth/middleware';
 
-// This function can be marked `async` if using `await` inside
-export async function middleware(request: NextRequest) {
-     const token = await getToken({req: request});
-     const url = request.nextUrl;
-     const isSpecifiedUrl = url.pathname.startsWith('/sign-in') ||
-                            url.pathname.startsWith('/sign-up') || 
-                            url.pathname.startsWith('/verify')  ||
-                            url.pathname.startsWith('/');
-
-     if(token && isSpecifiedUrl) {
-          return NextResponse.redirect(new URL('/dashboard', request.url))
-     }
-     // if(!token && url.pathname.startsWith('/dashboard')) {
-     //      return NextResponse.redirect(new URL('/sign-in', request.url))
-     // }
-}
- 
-// See "Matching Paths" below to learn more
 export const config = {
-  matcher: [
-     '/',
-     '/sign-in',
-     '/sign-up',
-     '/verify/:path*',
-     '/dashboard/:path*',
-  ],
+  matcher: ['/dashboard/:path*', '/sign-in', '/sign-up', '/', '/verify/:path*'],
+};
+
+export async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request });
+  const url = request.nextUrl;
+
+  // Redirect to dashboard if the user is already authenticated
+  // and trying to access sign-in, sign-up, or home page
+  if (
+    token &&
+    (url.pathname.startsWith('/sign-in') ||
+      url.pathname.startsWith('/sign-up') ||
+      url.pathname.startsWith('/verify') ||
+      url.pathname === '/')
+  ) {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
+  }
+
+  if (!token && url.pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL('/sign-in', request.url));
+  }
+
+  return NextResponse.next();
 }
